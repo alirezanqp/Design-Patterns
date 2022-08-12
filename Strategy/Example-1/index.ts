222117cb9dd444cecae9992769cb4642820ff3b6
Alireza Naghdipour <alirezanqp@gmail.com>
2022-08-12 14:10:07 +0000

    Add Example 1 for Strategy Design Pattern

---
+++ Strategy/Example-1/index.ts
@@ -0,0 +1,43 @@
+class Context {
+    private strategy: Strategy;
+
+    constructor(strategy: Strategy) {
+        this.strategy = strategy
+    }
+
+    public setStrategy(strategy: Strategy) {
+        this.strategy = strategy
+    }
+
+    public doSomeBusinessLogic(): void{
+        console.log('Context: Sorting data using the strategy')
+        const result = this.strategy.doAlgorithm(['a', 'b', 'c', 'd', 'e'])
+        console.log(result.join(','))
+    }
+}
+
+interface Strategy {
+    doAlgorithm(data: string[]): string[];
+}
+
+class ConcreteStrategyA implements Strategy {
+    doAlgorithm(data: string[]): string[] {
+        return data.sort();
+    }
+}
+
+class ConcreteStrategyB implements Strategy {
+    doAlgorithm(data: string[]): string[] {
+        return data.reverse();
+    }
+}
+
+const context = new Context(new ConcreteStrategyA());
+console.log('Client: Strategy is set to normal sorting.');
+context.doSomeBusinessLogic();
+
+console.log('');
+
+console.log('Client: Strategy is set to reverse sorting.');
+context.setStrategy(new ConcreteStrategyB());
+context.doSomeBusinessLogic()
